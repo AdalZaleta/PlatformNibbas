571132dd701ac9eaebadf51cc70579f60e0001bb
Retus001 <retus001@gmail.com>
2018-05-31 19:11:10 -0500

    CameraMan Update
    Simple Smoothboi

---
+++ Assets/_root/temp_placeholders/Camera_SmoothFollow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_SmoothFollow : MonoBehaviour {
+
+	public GameObject target;
+
+	public float smoothSpeed;
+	public Vector3 offset;
+
+	void FixedUpdate()
+	{
+		Vector3 finalPos = target.transform.position + offset;
+		Vector3 smoothedPos = Vector3.Lerp (transform.position, finalPos, smoothSpeed*Time.deltaTime);
+		if (finalPos.y <= 10)
+		{
+			Vector3 currentPos = new Vector3 (smoothedPos.x, finalPos.y, finalPos.z);
+			transform.position = currentPos;
+		}
+		if (finalPos.y > 10)
+		{
+			Vector3 currentPos = new Vector3 (smoothedPos.x, smoothedPos.y, finalPos.z);
+			transform.position = currentPos;
+		}
+	}
+}
