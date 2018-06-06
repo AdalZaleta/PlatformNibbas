2d4356e41dcc11ef4c7388be662390888a97e9d0
Retus001 <retus001@gmail.com>
2018-06-06 13:00:33 -0500

    Spikes
    Spikes send a mesage to player

---
+++ Assets/_root/Hazards/Spikes/Spikes.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes : MonoBehaviour {
+
+	public float dmg;
+
+	void OnCollisionEnter(Collision _col)
+	{
+		if (_col.gameObject.CompareTag("Player"))
+		{
+			_col.gameObject.SendMessage ("receiveDamage", dmg, SendMessageOptions.DontRequireReceiver);
+		}
+	}
+}
