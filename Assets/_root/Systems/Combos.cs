2fa26c2a949d5f1fe20570c6f3b8831f94ab5228
Retus001 <retus001@gmail.com>
2018-06-18 15:31:22 -0500

    Commit Fix [ works up to this point ]
    Commit Fix + Door Prefab  Scene Modify

---
+++ Assets/_root/Systems/Combos.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TAAI
+{
+	public class Combos : MonoBehaviour {
+
+		string DoorName;
+
+		private KeyCombo falconPunch = new KeyCombo(new string[] {"down", "right","right"});
+		private KeyCombo falconKick = new KeyCombo(new string[] {"down", "right","Fire1"});
+		private KeyCombo easteregg = new KeyCombo(new string[] {"left", "right", "left", "right", "Right_Trigger", "Right_Trigger", "Control_Y"});
+		private KeyCombo OGkonami = new KeyCombo(new string[] {"up", "up", "down", "down", "left", "right", "left", "right", "Control_B", "Control_A", "Control_Start"});
+		private KeyCombo key_0 = new KeyCombo(new string[] {"up", "down", "up"});
+		private KeyCombo key_1 = new KeyCombo(new string[] {"right", "left", "right"});
+		private KeyCombo key_2 = new KeyCombo(new string[] {"up", "down", "right", "left"});
+
+		public void SetDoorID(string _name)
+		{
+			DoorName = _name;
+		}
+
+		void Update () {
+			if (falconPunch.Check())
+			{
+				// do the falcon punch
+				Debug.Log("PUNCH"); 
+			}		
+			if (falconKick.Check())
+			{
+				// do the falcon punch
+				Debug.Log("KICK"); 
+			}
+			if (easteregg.Check())
+			{
+				Debug.Log ("EASTER EGG BOI");
+			}
+			if (OGkonami.Check())
+			{
+				Debug.Log ("KONAMI MODE ENGAGED !");
+			}
+			if (key_0.Check())
+			{
+				Debug.Log ("Used key key_0");
+				Manager_Static.locksManager.UseKey(DoorName, "key_0");
+			}
+			if (key_1.Check())
+			{
+				Debug.Log ("Used key key_1");
+				Manager_Static.locksManager.UseKey (DoorName, "key_1");
+			}
+			if (key_2.Check())
+			{
+				Debug.Log ("Used key key_2");
+				Manager_Static.locksManager.UseKey (DoorName, "key_2");
+			}
+		}
+	}
+}
