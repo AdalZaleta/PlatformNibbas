ce08e3a1ee5348712c5fc89add044c236adc3f4a
AdalZaleta <32682010+AdalZaleta@users.noreply.github.com>
2018-06-28 13:54:18 -0500

    Spawn Notas

---
+++ Assets/_root/Characters/Notes/Control_Notes.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control_Notes : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Invoke ("Desabilitar", 0.4f);
+	}
+
+	void Desabilitar()
+	{
+		gameObject.SetActive (false);
+	}
+}
