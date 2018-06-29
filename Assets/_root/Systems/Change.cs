edb215202a21aa328288960f977ef8e4e21ee850
AdalZaleta <32682010+AdalZaleta@users.noreply.github.com>
2018-06-28 20:41:57 -0500

    Dead Commit

---
+++ Assets/_root/Systems/Change.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TAAI
+{
+	public class Change : MonoBehaviour {
+
+		void Start () {
+			Invoke ("EndLoad", 5.0f);
+		}
+
+		void EndLoad()
+		{
+			Manager_Static.sceneManager.LoadSceneName ("cueva");
+		}
+	}
+}
